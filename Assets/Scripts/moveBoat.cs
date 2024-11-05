8e06fb27f7603d1dc2c26df8a866455b3c8c92a1
John James Gutib <jjgutib@gmail.com>
2024-11-05 17:07:19 +0800

    Prevent clipping through islands and subs

--- Assets/Scripts/moveBoat.cs
+++ Assets/Scripts/moveBoat.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityStandardAssets.Effects;
 
 public class moveBoat : MonoBehaviour
 {
-	public float upMovement;
+    public GameObject explosion;
+
+    public float upMovement;
 
 	private void Start()
 	{
@@ -13,4 +16,13 @@ public class moveBoat : MonoBehaviour
 		Vector3 a = new Vector3(0f, 0f, upMovement);
 		base.transform.position = base.transform.position + a * Time.deltaTime;
 	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Sub" || other.gameObject.tag == "Island")
+        {
+            Object.Instantiate(explosion, base.transform.position, explosion.transform.rotation);
+            UnityEngine.Object.Destroy(base.gameObject);
+        }
+    }
 }
